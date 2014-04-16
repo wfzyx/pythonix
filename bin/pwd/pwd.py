654e7d6502994a54e868e41aef89fdf3290d0b2f
bla <nathan_cruz77@hotmail.com>
2014-04-16 16:39:14 -0300

    Added PWD, as simple as it may seem

---
+++ bin/pwd/pwd.py
@@ -0,0 +1,6 @@
+# PWD - Print Working Directory - prints global path to current directory
+import os
+
+# I'm not sure if we can use this module, #
+# please let me know if we can't #
+print(os.getcwd())
