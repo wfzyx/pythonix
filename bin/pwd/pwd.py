7f1f7bf7c751b07db442775a53c30591f03aabdd
Bla <nathan_cruz77@hotmail.com>
2014-04-17 03:10:19 -0300

    Update pwd.py

--- bin/pwd/pwd.py
+++ bin/pwd/pwd.py
@@ -1,6 +1,5 @@
 # PWD - Print Working Directory - prints global path to current directory
+# TODO: Remove use of 'os' module
 import os
 
-# I'm not sure if we can use this module, #
-# please let me know if we can't #
 print(os.getcwd())
