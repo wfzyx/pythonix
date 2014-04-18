cf7985b728f2ca0d298176a8029167b6fbb10927
Victor Hugo Panisa <victor.panisa@gmail.com>
2014-04-18 17:01:53 -0300

    +KERNEL

--- kernel/main.py
+++ kernel/main.py
@@ -13,13 +13,22 @@ def bsp_finish_booting():
     krandom['random_sources'] = RANDOM_SOURCES
     krandom['random_elements'] = RANDOM_ELEMENTS
     
+    # MINIX is now ready. All boot image processes are on the ready queue.
+    # Return to the assembly code to start running the current process.
+    
+    
     # TODO check WTF is this
     #get_cpulocal_var(proc_ptr) = get_cpulocal_var_ptr(idle_proc)
     #get_cpulocal_var(bill_ptr) = get_cpulocal_var_ptr(idle_proc)
     announce()
     
+    # we have access to the cpu local run queue
+    # only now schedule the processes.
+    # We ignore the slots for the former kernel tasks
     for i in range(NR_BOOT_PROCS - NR_TASKS):
         RTS_UNSET(proc_addr(i), RTS_PROC_STOP)
+    
+    # enable timer interrupts and clock task on the boot CPU
     
     if(boot_cpu_init_timer(system_hz)):
         panic('''FATAL : failed to initialize timer interrupts,
@@ -55,6 +64,54 @@ def bsp_finish_booting():
     kernel_may_alloc = 0
     
     switch_to_user()
+
+
     
+def kmain(local_cbi={}):
+    ip = {}
+    rp = {}
+    
+    # TODO check if this is really necessary
+    kinfo = local_cbi
+    kmess = kinfo['kmess']
+    
+    machine['board_id'] = get_board_id_by_name(env_get(BOARDVARNAME))
+    
+    if __arm__:
+        arch_ser_init()
+    
+    # printing UP
+    print('PYTHONIX booting')
+    
+    kernel_may_alloc = 1
+    
+    assert(len(kinfo['boot_procs'] == len(image)))
+    kinfo['boot_procs'] = image
+    
+    cstart()
+    BKL_LOCK()
+    DEBUGEXTRA('main()')
+    proc_init()
+    
+    if(NR_BOOT_MODULES != kinfo['mbi']['mods_count'])
+        panic('expecting {} boot processes/modules, found {}'.format(
+                NR_BOOT_MODULES, kinfo['mbi']['mods_count']))
+                
+    # Setting up proc table entries for proc in boot image     
+    for i in range(NR_BOOT_PROCS):
+        ip = image[i]
+        debugextra('initializing {}'.format(ip['proc_name']))
+        rp = proc_addr(ip['proc_nr'])
+        ip['endpoint'] = rp['p_endpoint']
+        rp['p_cpu_time_left'] = 0
+        if(i < NR_TASKS):
+            rp['p_name'] = ip['proc_name']
+        else:
+            mb_mod = kinfo['module_list'][i - NR_TASKS]
+            ip['start_addr'] = mb_mod['mod_start']
+            # TODO check if this can be done with len()
+            ip['len'] = mb_mod['mob_end'] - mb_mod['mb_start']
+        
+        
     
 #if __name__ == '__main__':
