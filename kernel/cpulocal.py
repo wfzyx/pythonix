a434017ede72da3fd2d6068b8943fef244b227aa
bla <nathan_cruz77@hotmail.com>
2014-04-29 10:08:34 -0300

    Corrected some patterns with autopep8 (cpulocal and main) and some minor issues within main

--- kernel/cpulocal.py
+++ kernel/cpulocal.py
@@ -1,13 +1,12 @@
 class Cpulocal:
-	# TODO Check SMP override
-	def get_cpu_var(self,cpu=0,name):
-		return CPULOCAL_STRUCT[cpu][name]
+    # TODO Check SMP override
+    def get_cpu_var(self, name, cpu=0):
+        return CPULOCAL_STRUCT[cpu][name]
 
-	def get_cpulocal_var(self,name):
-		# TODO  check how cpuid is created
-		return get_cpu_var(cpuid,name)
+    def get_cpulocal_var(self, name):
+        # TODO  check how cpuid is created
+        return get_cpu_var(name, cpuid)
 
-	# MXCM #
-	# FIXME - padd the structure so that items in the array do not share
-	# cacheline with other CPUS
-	# TODO check the necessity of implementing local declarations
+    # MXCM #
+    # FIXME - padd the structure so that items in the array do not share
+    # cacheline with other CPUS
