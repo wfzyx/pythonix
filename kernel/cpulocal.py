0d3f63fabc8993e81e2017681a019c71a19eab32
Victor Hugo Panisa <victor.panisa@gmail.com>
2014-04-29 08:35:50 -0300

    KERNERL/CPULOCAL
    Started cpulocal class

--- kernel/cpulocal.py
+++ kernel/cpulocal.py
@@ -1,11 +1,13 @@
-# TODO Check SMP override
-def get_cpu_var(self,cpu=0,name):
-	return CPULOCAL_STRUCT[cpu][name]
+class Cpulocal:
+	# TODO Check SMP override
+	def get_cpu_var(self,cpu=0,name):
+		return CPULOCAL_STRUCT[cpu][name]
 
-def get_cpulocal_var(self,name):
-	# TODO  check how cpuid is created
-	return get_cpu_var(cpuid,name)
+	def get_cpulocal_var(self,name):
+		# TODO  check how cpuid is created
+		return get_cpu_var(cpuid,name)
 
-# MXCM #
-# FIXME - padd the structure so that items in the array do not share
-# cacheline with other CPUS
+	# MXCM #
+	# FIXME - padd the structure so that items in the array do not share
+	# cacheline with other CPUS
+	# TODO check the necessity of implementing local declarations
