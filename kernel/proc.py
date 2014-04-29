33b6c6a3a5af05d60871bd25a85f59bd79e2d84f
bla <nathan_cruz77@hotmail.com>
2014-04-29 02:55:26 -0300

    +KERNEL/PROC.PY

--- kernel/proc.py
+++ kernel/proc.py
@@ -97,8 +97,8 @@ def __idle():
     spent not doing anything. This allows test setups to measure
     the CPU utilization of certain workloads with high precision.'''
 
-    get_cpulocal_var(proc_ptr) = get_cpulocal_var_ptr(idle_proc)
-    p = get_cpulocal_var_ptr(idle_proc)
+    # TODO: Check how to handle this in python
+    # p = get_cpulocal_var(proc_ptr) = get_cpulocal_var_ptr(idle_proc)
 
     if priv(p)['s_flags'] & BILLABLE:
         get_cpulocal_var(bill_ptr) = p
@@ -153,7 +153,7 @@ def __do_sync_ipc(caller_ptr, call_nr, src_dst_e, m_ptr):
         callname != ipc_call_names[call_nr]
     ):
         if DEBUG_ENABLE_IPC_WARNINGS:
-            print('sys_call: trap {} not_allowed, caller {}, src_dst {}' \
+            print('sys_call: trap {} not_allowed, caller {}, src_dst {}'
                   .format(call_nr, proc_nr(caller_ptr), src_dst_e))
         return ETRAPDENIED
 
@@ -172,9 +172,9 @@ def __do_sync_ipc(caller_ptr, call_nr, src_dst_e, m_ptr):
         if call_nr != RECEIVE:
             if not may_send_to(caller_ptr, src_dst_p):
                 if DEBUG_ENABLE_IPC_WARNINGS:
-                    print('sys_call: ipc mask denied {} from {} to {}'\
+                    print('sys_call: ipc mask denied {} from {} to {}'
                           .format(callname, caller_ptr['p_endpoint'],
-                          src_dst_e))                          
+                                  src_dst_e))
                 return ECALLDENIED
 
     # MXCM #
@@ -184,13 +184,13 @@ def __do_sync_ipc(caller_ptr, call_nr, src_dst_e, m_ptr):
 
     if not priv(caller_ptr)['s_trap_mask'] & (1 << call_nr):
         if DEBUG_ENABLE_IPC_WARNINGS:
-            print('sys_call: ipc mask denied {} from {} to {}'\
+            print('sys_call: ipc mask denied {} from {} to {}'
                   .format(callname, caller_ptr['p_endpoint'], src_dst_e))
         return ETRAPDENIED
 
     if call_nr != SENDREC and call_nr != RECEIVE and iskerneln(src_dst_p):
         if DEBUG_ENABLE_IPC_WARNINGS:
-            print('sys_call: ipc mask denied {} from {} to {}'\
+            print('sys_call: ipc mask denied {} from {} to {}'
                   .format(callname, caller_ptr['p_endpoint'], src_dst_e))
         return ETRAPDENIED
 
@@ -200,7 +200,7 @@ def __do_sync_ipc(caller_ptr, call_nr, src_dst_e, m_ptr):
     elif call_nr == SEND:
         result = mini_send(caller_ptr, src_dst_e, m_ptr, 0)
         if call_nr == SEND or result != OK:
-            pass       
+            pass
         # TODO tweak logic to swcase break
         # TODO tweak logic to swcase fall
     elif call_nr == RECEIVE:
@@ -217,3 +217,95 @@ def __do_sync_ipc(caller_ptr, call_nr, src_dst_e, m_ptr):
 
     # Return the result of system call to the caller #
     return result
+
+
+def do_ipc(r1, r2, r3):
+    # TODO: Check if this way of translating pointer is right
+    caller_ptr = get_cpulocal_var(proc_ptr)
+    caller_nr = r1
+
+    assert(not RTS_ISSET(caller_ptr, RTS_SLOT_FREE))
+
+    # MXCM #
+    # Bill kernel time to this process
+    kbill_ipc = caller_ptr
+
+    # MXCM #
+    # If this process is subset to system call tracing,
+    # handle that first
+
+    if caller_ptr['p_misc_flags'] & (MF_SC_TRACE | MR_SC_DEFER):
+        # MXCM #
+        # Are we tracing this process, and is it the
+        # first sys_call entry?
+
+        if (caller_ptr['p_misc_flags'] & (MF_SC_TRACE | MR_SC_DEFER)) == MF_SC_TRACE:
+            # MXCM #
+            '''We must notify the tracer before processing the actual
+            system call. If we don't, the tracer could not obtain the
+            input message. Postpone the entire system call.'''
+
+            caller_ptr['p_misc_flags'] &= MF_SC_TRACE * -1
+            assert(not caller_ptr['p_misc_flags'] & MR_SC_DEFER)
+            caller_ptr['p_misc_flags'] |= MF_SC_DEFER
+            caller_ptr['p_defer']['r1'] = r1
+            caller_ptr['p_defer']['r2'] = r2
+            caller_ptr['p_defer']['r3'] = r3
+
+            # Signal the "enter system call" event. Block the process.
+            cause_sig(proc_nr(caller_ptr), SIGTRAP)
+
+            # Preserve the return registrer's value.
+            return caller_ptr['p_reg']['retreg']
+
+        # If the MF_SC_DEFER flag is set, the syscall is now being resumed.
+        caller_ptr['p_misc_flags'] &= MF_SC_DEFER * -1
+        assert(not caller_ptr['p_misc_flags'] & MF_SC_ACTIVE)
+
+        # Set a flag to allow reliable tracing of leaving the system call.
+        caller_ptr['p_misc_flags'] |= MF_SC_ACTIVE
+
+    if caller['p_misc_flags'] & MF_DELIVERMSG:
+        panic('sys_call: MF_DELIVERMSG on for {0} / {1}'
+              .format(caller_ptr['p_name'], caller_ptr['p_endpoint']))
+
+    # MXCM #
+    '''Now check if the call is known and try to perform the request. The only
+    system calls that exist in MINIX are sending and receiving messages.
+    - SENDREC: combines SEND and RECEIVE in a single system call
+    - SEND:    sender blocks until its message has been delivered
+    - RECEIVE: receiver blocks until an acceptable message has arrived
+    - NOTIFY:  asynchronous call; deliver notification or mark pending
+    - SENDA:   list of asynchronous send requests'''
+
+    if call_nr == SENDNB:
+        # Process accounting for scheduling
+        # TODO: Check castings here
+        return __do_sync_ipc(caller_ptr, call_nr, r2, r3)
+
+    elif call_nr == SENDA:
+        # Get and check the size of the arguments in bytes
+        # TODO: Check if len() get the needed size from r2
+        msg_size = len(r2)
+
+        # Process accounting for scheduling
+        caller_ptr['p_accounting']['ipc_async'] += 1
+
+        # Limit size to something reasonable. An arbitrary choice is 16
+        # times the number of process table entries
+        if msg_size > 16*(NR_TASKS + NR_PROCS):
+            return EDOM
+        # TODO: Check castings here
+        return mini_senda(caller_ptr, r3, msg_size)
+
+    elif call_nr == PYTHONIX_KERNINFO:
+        # It may not be initialized yet
+        if not pythonix_kerninfo_user:
+            return EBADCALL
+
+        arch_set_secondary_ipc_return(caller_ptr, pythonix_kerninfo_user)
+        return OK
+
+    else:
+        # Illegal system call
+        return EBADCALL
