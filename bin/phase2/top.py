a245d35cee420c7ee05e38162c7e8b7d9060fd72
vhpanisa <victor.panisa@gmail.com>
2015-07-14 17:41:02 +0000

    almost finished top.py, only missing cpu time and system info

--- bin/phase2/top.py
+++ bin/phase2/top.py
@@ -4,6 +4,13 @@ from __future__ import print_function
 import sys
 import os
 
+users = {}
+with open('/etc/passwd', 'r') as f:
+  for x in f:
+    aux = x.split(':')
+    users[aux[2]] = aux[0]
+
+
 def _top():
     rows = int(os.popen('stty', 'r').read().split(' ')[0])
     headers = ['psi_v','type','endpoint','name','state','blocked','priority',
@@ -12,8 +19,8 @@ def _top():
         'nicevalue', 'vfsblock', 'blockproc', 'ctrltty', 'kipchi', 'kipclo', 
         'kcallhi', 'kcalllo']
 
-    txtheader = ['pid','realuid','nicevalue','tmemory','priority','state',
-        'utime','utime','name']
+    txtheader = ['pid','realuid','priority','nicevalue','tmemory','state',
+      'utime','execcycleslo','name']
 
     procs = [id for id in os.listdir('/proc') if id.isdigit()]
     topdata = []    
@@ -32,11 +39,46 @@ def _top():
     print('something about memory')
     print('cpu line 1')
     print('cpu line 2')
-    print('{0: <5} {1: >8} {2: <3} {3: <7} {4: >5} {5: <6} {6: <7} {7: >7}'.format('PID', 'USERNAME', 'PRI', 'NICE', 'SIZE', 'STATE', 'TIME', 'CPU COMMAND'))
+    print()
+    print('{0: >5} {1: <8} {2: >3} {3: >4} {4: >5} {5: >5} {6: >6} {7: >7} {8: >7}'.format('PID', 'USERNAME', 'PRI', 'NICE', 'SIZE', 'STATE', 'TIME', 'CPU', 'COMMAND'))
 
-    for proc,i in zip(topdata,range(7,rows)):
+    for proc,i in zip(topdata,range(8,rows)):
         for txt in txtheader:
-            print('{:}'.format(proc[txt]), end='|')
+            if txt == 'pid':
+              s = '{0: >5}'
+              value = proc[txt]
+            elif txt == 'realuid':
+              s = '{0: <8}'
+              value = users[proc[txt]]
+            elif txt == 'priority':
+              s = '{0: >3}'
+              value = proc[txt]
+            elif txt == 'nicevalue':
+              s = '{0: >4}'
+              value = proc[txt]
+            elif txt == 'tmemory':
+              s = '{0: >5}'
+              value = str(int(proc[txt])/1024)+'k'
+            elif txt == 'state':
+              s = '{0: >5}'
+              value = 'RUN' if proc[txt] == 'R' else ''
+            elif txt == 'utime':
+              s = '{0: >6}'
+              secs = int(proc[txt])
+              mins = secs/60
+              if mins >= 60:
+                hours = mins//60
+                mins = mins%60
+              else:
+                hours = 0
+              value = str(hours)+':'+'{0:0>2}'.format(str(mins))
+            elif txt == 'execcycleslo':
+              s = '{0: >6}%'
+              value = proc[txt] if txt in proc else '0.00'
+            else:
+              s = '{0}'
+              value = proc[txt]
+            print(s.format(value), end=' ')
         print()
 
 def loopTop():
